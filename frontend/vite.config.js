69ba1c8a9f6c9eb7bab1165da74f47318f33b789
Riju Saha <rijus289@gmail.com>
2025-07-14 03:04:49 +0530

    Adjust chunk size limit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
-import { visualizer } from 'rollup-plugin-visualizer';
+import { visualizer } from "rollup-plugin-visualizer";
 export default defineConfig({
-  plugins: [react(),tailwindcss(),visualizer({ open: true })],
- 
-})
+  plugins: [react(), tailwindcss(), visualizer({ open: true })],
+  build: {
+    chunkSizeWarningLimit: 1500, // increase limit to 1500kb (default is 500kb)
+  },
+});
